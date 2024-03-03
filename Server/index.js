7461d24e47a65fec7de2d9099b73cf608777bedd
thimanshu756 <himanshu1662.be22@gmail.com>
2024-03-04 00:39:45 +0530

    basic index file created

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,22 @@
-hii there
+const express = require("express");
+const app = express();
+const dotenv= require("dotenv");
+const database  = require("./config/database")
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+database.connect();
+
+app.get("/",(req,res)=>{
+    return res.json({
+        success:true,
+        message:"your server is up and running"
+    })
+})
+
+app.listen(PORT , ()=>{
+    console.log(`App is running at ${PORT}`);
+})
